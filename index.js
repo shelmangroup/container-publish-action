ecb2aa747979901d38fb293801112cf42f1b0964
Daniel Lundin <daniel@arity.se>
2020-03-26 19:21:02 +0100

    js hacketihack

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { exec } = require("child_process");
 
 var script = `
 /bin/bash -c <EOF
+ . /etc/os-release
   sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${ID^}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
   wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${ID^}_${VERSION_ID}/Release.key -O Release.key
   sudo apt-key add - < Release.key
