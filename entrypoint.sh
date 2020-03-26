b5b047dfc6c42c50303bfc9dbacd584bd18cc593
Daniel Lundin <daniel@arity.se>
2020-03-26 19:09:14 +0100

    test using javacsript variant

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 function main(){
 
+  install_deps
+
   REGISTRY_NO_PROTOCOL=$(echo "${INPUT_REGISTRY}" | sed -e 's/^https:\/\///g')
   if uses "${INPUT_REGISTRY}" && ! isPartOfTheName "${REGISTRY_NO_PROTOCOL}"; then
     INPUT_NAME="${REGISTRY_NO_PROTOCOL}/${INPUT_NAME}"
@@ -58,6 +60,15 @@ function build_and_push() {
   do
     echo buildah push "${INPUT_NAME}:${TAG}"
   done
+}
+
+function install_deps() {
+  . /etc/os-release
+  sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${ID^}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+  wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${ID^}_${VERSION_ID}/Release.key -O Release.key
+  sudo apt-key add - < Release.key
+  sudo apt-get update -qq
+  sudo apt-get -qq -y install buildah
 }
 
 main
