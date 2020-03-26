e217038cbf04fdd780784e998ea58682d3d08b65
Daniel Lundin <daniel@arity.se>
2020-03-26 19:16:08 +0100

    js hacketihack

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const { exec } = require("child_process");
 
-exec("entrypoint.sh", (error, stdout, stderr) => {
+exec( <<"HERE"
+/bin/bash -c <EOF
+  sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${ID^}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+  wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${ID^}_${VERSION_ID}/Release.key -O Release.key
+  sudo apt-key add - < Release.key
+  sudo apt-get update -qq
+  sudo apt-get -qq -y install buildah
+
+EOF
+HERE
+, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
