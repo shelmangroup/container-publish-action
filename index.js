359817870c4f13e30119ea4467412a4ba66ed11e
Daniel Lundin <daniel@arity.se>
2020-03-26 19:27:04 +0100

    js hacketihack

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { exec } = require("child_process");
 var script = `
 /bin/bash -c <EOF
  . /etc/os-release
-  sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${ID^}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-  wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${ID^}_${VERSION_ID}/Release.key -O Release.key
+  sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x\${ID^}_\${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+  wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x\${ID^}_\${VERSION_ID}/Release.key -O Release.key
   sudo apt-key add - < Release.key
   sudo apt-get update -qq
   sudo apt-get -qq -y install buildah
